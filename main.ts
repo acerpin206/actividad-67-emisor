95873d69745c7c232b1f14a5f256e4986987e250
acerpin206 <132546867+acerpin206@users.noreply.github.com>
2023-05-11 16:34:40 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,30 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    led.setBrightness(led.brightness() + receivedNumber)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(-25)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("Power")
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Power") {
+        encendido = !(encendido)
+        led.setBrightness(125)
+        led.enable(encendido)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(25)
 })
+let encendido = false
 radio.setGroup(1)
-basic.forever(function () {
-	
-})
+encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
